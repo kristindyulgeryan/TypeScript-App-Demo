1e386b31f226d5078ff7708f60561c547ebfac2a
Kristin <kristindyulgeryan@abv.bg>
2025-05-20 15:04:38 +0300

    Create context API with react and typescript

---
+++ react-ts/src/store/todos-context.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Todo from "../models/todo";
+
+type TodoContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = React.createContext<TodoContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC<React.PropsWithChildren<{}>> = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const onRemoveTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodoContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: onRemoveTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
